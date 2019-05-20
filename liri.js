c509ada0fc59edcc7defbbf4345634c8994ede34
Stephen Malito <stevemalito@gmail.com>
2019-05-20 17:22:51 -0500

    liri.js copy commands needed

--- liri.js
+++ liri.js
@@ -1,2 +1,14 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+
+
+var spotify = new Spotify(keys.spotify);
+
+// Liri comands
+// `concert-this`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
