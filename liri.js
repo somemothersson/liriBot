257b66736e89d525864655667dacf52d7507bac8
Stephen Malito <stevemalito@gmail.com>
2019-05-20 17:32:57 -0500

    liri.js built out switch case

--- liri.js
+++ liri.js
@@ -4,22 +4,31 @@ let operator = process.argv[2]
 let funcer = process.argv[3]
 let spotify = new Spotify(keys.spotify);
 
-// Liri comands
-// `concert-this`
 
-// * `spotify-this-song`
+switch (operator) {
+    case "concert-this":
+        bIt()
+        break;
+    case "spotify-this-song":
+        spotify()
+        break;
+    case "movie-this":
+        movie()
+        break;
+    case "do-what-it-says":
+        doIt()
+        break;
+  
 
-// * `movie-this`
 
-// * `do-what-it-says`
+}
+function bIt (){
 
+}
 function spotify (){
 
 }
 
-function bIt (){
-
-}
 function movie(){
 
 }
