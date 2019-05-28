f03a04fc8d0ad5bc801bf23647af184942e4daf2
Stephen Malito <stevemalito@gmail.com>
2019-05-28 13:09:42 -0500

    spotify now working

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ let fs = require("fs");
 let axios = require("axios");
 let keys = require("./keys.js");
 let operator = process.argv[2]
-let funcer = process.argv.slice(3).join(" ");
+let funker = process.argv.slice(3).join(" ");
 let spotify = new Spotify(keys.spotify);
 
 
@@ -27,15 +27,14 @@ switch (operator) {
 }
 function bIt (){
 
-    axios.get(`https://rest.bandsintown.com/artists/${funcer}/events?app_id=codingbootcamp`).then(
+    axios.get(`https://rest.bandsintown.com/artists/${funker}/events?app_id=codingbootcamp`).then(
   function(response) {
       
         console.log(`
         *******************
-        Concert Listings for ${funcer}
+        Concert Listings for ${funker}
         `)
       for (i = 0; i <response.data.length; i++){
-        console.log(response.data[i].datetime)
         let venue = response.data[i].venue.name
         let city = response.data[i].venue.city
         let state = response.data[i].venue.state
@@ -44,29 +43,29 @@ function bIt (){
         ${dateTime}
         ${venue}
         ${city}, ${state}
-
-        
-        
         `)
-
       }
 
-    
-    // console.log(response);
-
-    // * Name of the venue
-
-    // * Venue location
-
-    // * Date of the Event (use moment to format this as "MM/DD/YYYY")
   }
 );
 
 }
 
-// function spotifySearch (){
+function spotifySearch (){
+  spotify.search({ type: 'track', query: funker }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  console.log(`
+  Artist(s): ${data.tracks.items[0].album.artists[0].name}
+  Song Name: ${data.tracks.items[0].album.name}
+  Preview Link: ${data.tracks.items[0].album.external_urls.spotify}
+  Album Title: ${data.tracks.items[0].name}
+  `);
 
-// }
+  });
+
+}
 
 // function movie(){
 //     axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
